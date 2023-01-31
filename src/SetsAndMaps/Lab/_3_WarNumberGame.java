85de89ecbbe71cbf17c08d545d3c41cf233b8f44
Kaloy4n <kaloyan.braskov@gmail.com>
2023-02-01 00:26:41 +0200

    Submitting a solution of WarNumberGame exercise.

---
+++ src/SetsAndMaps/Lab/_3_WarNumberGame.java
@@ -0,0 +1,52 @@
+package SetsAndMaps.Lab;
+
+import java.util.*;
+
+public class _3_WarNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Integer> deckOne = new LinkedHashSet<>();
+        Set<Integer> deckTwo = new LinkedHashSet<>();
+
+        String[] inputOne = scanner.nextLine().split(" ");
+        for (String card : inputOne) {
+            int current = Integer.parseInt(card);
+            deckOne.add(current);
+        }
+
+        String[] inputTwo = scanner.nextLine().split(" ");
+        for (String card : inputTwo) {
+            int current = Integer.parseInt(card);
+            deckTwo.add(current);
+        }
+
+        int step = 1;
+        while(step <= 50) {
+            // fight logic
+
+            Integer fighterOne = deckOne.iterator().next();
+            deckOne.remove(fighterOne);
+
+            Integer fighterTwo = deckTwo.iterator().next();
+            deckTwo.remove(fighterTwo);
+
+            if (fighterOne > fighterTwo) {
+                deckOne.add(fighterOne);
+                deckOne.add(fighterTwo);
+            } else if (fighterTwo > fighterOne) {
+                deckTwo.add(fighterOne);
+                deckTwo.add(fighterTwo);
+            }
+            step++;
+        }
+
+        if(deckOne.size() < deckTwo.size()) {
+            System.out.println("Second player win!");
+        } else if (deckOne.size() > deckTwo.size()) {
+            System.out.println("First player win!");
+        } else {
+            System.out.println("Draw!");
+        }
+    }
+}
