3bd5f61beaf5c5c16ea4f1eee9402f35aa9c56fc
Kaloy4n <kaloyan.braskov@gmail.com>
2023-02-11 13:49:08 +0200

    Submitting a solution of CopyFile exercise.

---
+++ src/StreamsFilesAndDirectories/Lab/P3CopyFile.java
@@ -0,0 +1,33 @@
+package StreamsFilesAndDirectories.Lab;
+
+import java.io.*;
+
+public class P3CopyFile {
+    public static void main(String[] args) {
+        String basePath = "D:\\JAVA\\SoftUni\\Java Advanced Jan23\\04. Java-Advanced-Files-and-Streams-Lab-Resources";
+        String inputPath = basePath + "\\input.txt";
+        String outputPath = basePath + "\\copy_output.txt";
+
+        try(InputStream inputStream = new FileInputStream(inputPath)) {
+            try(OutputStream outputStream = new FileOutputStream(outputPath)) {
+
+                int readByte;
+                while ((readByte = inputStream.read()) >= 0) {
+                    // == 10 or == 32 ASCII char codes
+                    if (readByte == ' ' || readByte == '\n') {
+                        outputStream.write(readByte);
+                    } else {
+                        String number = String.valueOf(readByte);
+                        for (int i = 0; i < number.length(); i++) {
+                            outputStream.write(number.charAt(i));
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
