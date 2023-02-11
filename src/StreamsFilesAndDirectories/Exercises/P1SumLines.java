3364085bedd65372d776fbede5eddf4f879dbe62
Kaloy4n <kaloyan.braskov@gmail.com>
2023-02-11 19:14:04 +0200

    Adding txt resource files to work with in solving exercises and submitting the first SumLines exercise

---
+++ src/StreamsFilesAndDirectories/Exercises/P1SumLines.java
@@ -0,0 +1,34 @@
+package StreamsFilesAndDirectories.Exercises;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class P1SumLines {
+    public static void main(String[] args) throws IOException {
+        //1. Get all rows from input.txt file
+        String pathToFile = "D:\\JAVA\\SoftUni\\Java Advanced Jan23\\Java Advanced Jan23\\src\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        List<String> allLines = Files.readAllLines(Path.of(pathToFile));
+
+/*        2. For each row find the sum of the ASCII chars and print it
+*        2.1 one way to solve it
+*        for (String line : allLines) {
+*            //line = "Michael Angelo,"
+*            int sum = 0;
+*            for (char symbol : line.toCharArray()) {
+*                sum += symbol;
+*            }
+*            System.out.println(sum);
+*        }
+*/
+        //2.2 second way to solve it
+        allLines.forEach(line -> {
+            int sum = 0;
+            for (char symbol : line.toCharArray()) {
+                sum += symbol;
+            }
+            System.out.println(sum);
+        });
+    }
+}
