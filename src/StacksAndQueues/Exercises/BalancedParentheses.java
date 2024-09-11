573a119e00e6140bcfc921f13bfad27503c84755
Kalo4an <kaloyan.braskov@gmail.com>
2024-09-11 22:49:56 +0300

    Add a solution to Stack and Queue exercises.

--- src/StacksAndQueues/Exercises/BalancedParentheses.java
+++ src/StacksAndQueues/Exercises/BalancedParentheses.java
@@ -13,34 +13,28 @@ public class BalancedParentheses {
 
         boolean areSymbolsBalanced = false;
 
-        label:
         //"{[()]}" -> .toCharArray() -> ['{', '[', '('...]
         for (char symbol : input.toCharArray()) {
             switch (symbol) {
-                case '(':
-                case '[':
-                case '{':
+                case '(', '[', '{' ->
                     //open {, [, ( -> add to stack i.e. LIFO
                     openBracketsStack.push(symbol);
-                    break;
-                case ')':
-                case ']':
-                case '}':
+
+                case ')', ']', '}' ->
                     //closed ), ], } -> add to queue i.e. FIFO
                     closedBracketsQueue.offer(symbol);
-                    break;
-                default:
-                    break label;
             }
             if(!openBracketsStack.isEmpty() && !closedBracketsQueue.isEmpty()) {
-                if (openBracketsStack.peek() == '(' && closedBracketsQueue.peek() == ')' || openBracketsStack.peek() == '[' && closedBracketsQueue.peek() == ']' || openBracketsStack.peek() == '{' && closedBracketsQueue.peek() == '}') {
+                if (openBracketsStack.peek() == '(' && closedBracketsQueue.peek() == ')'
+                        || openBracketsStack.peek() == '[' && closedBracketsQueue.peek() == ']'
+                        || openBracketsStack.peek() == '{' && closedBracketsQueue.peek() == '}') {
                     areSymbolsBalanced = true;
                     openBracketsStack.pop();
                     closedBracketsQueue.poll();
                 } else {
                     areSymbolsBalanced = false;
                 }
-            } else if (openBracketsStack.isEmpty() || closedBracketsQueue.isEmpty()) {
+            } else {
                 areSymbolsBalanced = false;
             }
         }
