4596574aa6bd245159f1e339c0a41f82e8bf4fd5
Kalo4an <kaloyan.braskov@gmail.com>
2024-09-11 22:56:47 +0300

    Add a solution to Stack and Queue exercises.

--- src/StacksAndQueues/Exercises/RecursiveFibonacci.java
+++ src/StacksAndQueues/Exercises/RecursiveFibonacci.java
@@ -7,7 +7,7 @@ public class RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        memory = new long[n + 1];
+        memory = new long[n + 1]; // to be able to collect all numbers n
 
         long result = fib(n);
         System.out.println(result);
@@ -16,6 +16,10 @@ public class RecursiveFibonacci {
 private static long fib(int n) {
     if (n < 2) {
         return 1;
+    }
+
+    if (memory[n] != 0) {
+        return memory[n];
     }
 
     memory[n] = fib(n - 1) + fib(n - 2);
